6b0a85aeeac5002f63b29b553e1b6896256ef481
iamhmjawad <thisishmjawad@gmail.com>
2023-12-14 14:24:51 +0500

    12/14/2023

---
+++ 53_maximumSubarray/linear_max_SubArray.py
@@ -0,0 +1,17 @@
+
+from typing import List
+
+
+def maxSubArray(nums: List[int]) -> int:
+    maxSub = nums[0]
+    currSum = 0
+    for n in nums:
+        if currSum < 0:
+            currSum = 0
+        currSum += n
+        maxSub = max(currSum, maxSub)
+    return maxSub
+
+
+result = maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+print("Maximum Subarray Sum:", result)
